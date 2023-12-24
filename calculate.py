63d6cf081fd0f7fdcb7f55bcf1692624544d3f91
Dv04 <62090715+Dv04@users.noreply.github.com>
2023-12-24 20:56:49 +0530

    Update .gitignore and add Jaya algorithm implementation

--- calculate.py
+++ calculate.py
@@ -30,7 +30,7 @@ def calculate_final_demand(capacity_dict):
 def generate_final_demand(capacity_dict):
     result = calculate_final_demand(capacity_dict)
     while not (
-        capacity_dict["min_capacity"][5] < result[5] < capacity_dict["max_capacity"][4]
+        capacity_dict["min_capacity"][5] < result[5] < capacity_dict["max_capacity"][5]
     ):
         result = calculate_final_demand(capacity_dict)
     return result
@@ -39,7 +39,7 @@ def generate_final_demand(capacity_dict):
 def generate_final_demand_array(capacity_dict):
     demand_array = np.empty((100, 145))
     for row in range(100):
-        for hour in range(0, 24):
+        for hour in range(24):
             capacity_dict["final_demand"] = capacity_dict["hour_demand"][hour]
             demand = generate_final_demand(capacity_dict)
             demand_array[row, (hour) * 6 : (hour + 1) * 6] = demand[:6]
@@ -57,9 +57,10 @@ def generate_final_demand_array(capacity_dict):
     return demand_array
 
 
-final_array = generate_final_demand_array(capacity_dict)
-print(final_array)
+if __name__ == "__main__":
+    final_array = generate_final_demand_array(capacity_dict)
+    print(final_array)
 
-# save output as csv file
-df = pd.DataFrame(final_array)
-df.to_csv("final_demand.csv")
+    # save output as csv file
+    df = pd.DataFrame(final_array)
+    df.to_csv("final_demand.csv")
