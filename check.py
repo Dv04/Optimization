b7886e9b2f41f2341970671c5d2084a398772787
Dv04 <62090715+Dv04@users.noreply.github.com>
2023-12-25 16:37:45 +0530

    Update capacity_dict and check_output function

--- check.py
+++ check.py
@@ -3,6 +3,7 @@ import csv
 import numpy as np
 from constants import capacity_dict
 
+
 def check_output():
     with open("best_solution_new.csv", newline="") as csvfile:
         reader = csv.reader(csvfile, delimiter=",")
@@ -13,10 +14,34 @@ def check_output():
         falseCtn = 0
 
         for i in range(len(data) - 1):
-            if (capacity_dict["min_capacity"][i % 6] <=  data[i] <= capacity_dict["max_capacity"][i % 6]) and data[i] > 0:
-                print("\033[92m" + "True for index: ", i, " | ", data[i], " | Wanted: [", capacity_dict["min_capacity"][i % 6], ",", capacity_dict["max_capacity"][i % 6], "]" + "\033[0m")
+            if (
+                (capacity_dict["min_capacity"][i % 6] - 0.1)
+                <= data[i]
+                <= capacity_dict["max_capacity"][i % 6]
+            ) and data[i] > 0:
+                print(
+                    "\033[92m" + "True for index: ",
+                    i,
+                    " | ",
+                    data[i],
+                    " | Wanted: [",
+                    (capacity_dict["min_capacity"][i % 6] - 0.1),
+                    ",",
+                    capacity_dict["max_capacity"][i % 6],
+                    "]" + "\033[0m",
+                )
             else:
-                print("False for index: ", i, " | ", data[i], " | Wanted: [", capacity_dict["min_capacity"][i % 6], ",", capacity_dict["max_capacity"][i % 6], "]")
+                print(
+                    "False for index: ",
+                    i,
+                    " | ",
+                    data[i],
+                    " | Wanted: [",
+                    (capacity_dict["min_capacity"][i % 6] - 0.1),
+                    ",",
+                    capacity_dict["max_capacity"][i % 6],
+                    "]",
+                )
                 falseCtn += 1
 
         if falseCtn == 0:
@@ -32,7 +57,13 @@ def check_output():
             temp = data[i : i + 6]
             if sum(map(float, temp)) == capacity_dict["hour_demand"][i // 6]:
                 ctn += 1
-                
-            print("New solution sum: ", sum(map(float, temp)), '| Wanted: ', capacity_dict["hour_demand"][i // 6], " ", )
+
+            print(
+                "New solution sum: ",
+                sum(map(float, temp)),
+                "| Wanted: ",
+                capacity_dict["hour_demand"][i // 6],
+                " ",
+            )
 
         print("\n\nHour demand matched: ", ctn, "times.")
